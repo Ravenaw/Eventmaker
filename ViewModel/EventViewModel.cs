adf7950258aca99a8f2611087cec9df730a347b1
Ani Vladislavova Boyadzhieva <anix0072@edu.easj.dk>
2020-02-03 12:29:35 +0100

    1.7

--- ViewModel/EventViewModel.cs
+++ ViewModel/EventViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Eventmaker.Model;
 
 namespace Eventmaker.ViewModel
 {
     class EventViewModel
     {
-
+        private EventCatalogSingleton instance;
+        public EventViewModel()
+        {
+            instance=EventCatalogSingleton.Instance;
+        }
     }
 }
