f3c288775c35465b936429288b7deb7d6776790b
Marcell Varga <ravenaw@gmail.com>
2020-02-03 10:47:12 +0100

    -

---
+++ Model/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventmaker.Model
+{
+    class Event
+    {
+        private int ID;
+        private string Name;
+        private string Description;
+        private string Place;
+        private DateTime DateTime;
+    }
+}
