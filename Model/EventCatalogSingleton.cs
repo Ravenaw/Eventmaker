6ec9b42a3927e00dedc167df39575a0d7e876589
Ani Vladislavova Boyadzhieva <anix0072@edu.easj.dk>
2020-02-03 10:56:30 +0100

    Event Catalog Singleton

---
+++ Model/EventCatalogSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventmaker.Model
+{
+    class EventCatalogSingleton
+    {
+        private ObservableCollection<Event> EventCatalog;
+
+        EventCatalogSingleton()
+        {
+            
+        }
+
+        public void Add(Event e)
+        {
+            EventCatalog.Add(e);
+        }
+    }
+}
