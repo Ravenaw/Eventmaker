84f099028bf9ea4efaf0298e2ca9c4d33f222696
Ani Vladislavova Boyadzhieva <anix0072@edu.easj.dk>
2020-02-04 18:20:02 +0100

    .

--- Handler/EventHandler.cs
+++ Handler/EventHandler.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Eventmaker.Converter;
+using Eventmaker.Model;
+using Eventmaker.View;
+using Eventmaker.ViewModel;
 
 namespace Eventmaker.Handler
 {
     class EventHandler
     {
+        public EventViewModel Handler { get; set; }
 
+        public EventHandler(EventViewModel evm)
+        {
+            Handler = evm;
+        }
+
+        public void CreateEvent()
+        {
+            Event e = new Event(Handler.ID, Handler.Name, Handler.Description, Handler.Place, DateTimeConverter.DateTimeOffsetAndTimeSetToDateTime(Handler.Date, Handler.Time));
+            Handler.Instance.Add(e);
+        }
     }
 }
