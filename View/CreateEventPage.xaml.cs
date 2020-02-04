cbd6e461b90bd602e5129e353aa88f1baf6fec9a
Marcell Varga <ravenaw@gmail.com>
2020-02-04 21:13:16 +0100

    trying hard or hardly trying

--- View/CreateEventPage.xaml.cs
+++ View/CreateEventPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Eventmaker.ViewModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -30,6 +31,11 @@ namespace Eventmaker.View
         private void GoToEventPage(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(EventPage));
+        }
+
+        private void CreateEvent(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
